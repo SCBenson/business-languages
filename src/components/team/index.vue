120a0a95c1b2fa56a15d07d6fc61f98aa3d15859
Sean Benson Guerra <sean_c_benson@outlook.com>
2025-01-26 14:55:42 +0000

    Created a card system that imports a data constant with objects that have information on every team member. A Show more/less button was made to prompt user to keep reading. The min-length is 500 chars.

--- src/components/team/index.vue
+++ src/components/team/index.vue
@@ -1,14 +1,62 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-card min-height="200">
-                
-            </v-card>
-        </v-row>
-    </v-container>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card v-for="(member, index) in data" :key="index" class="mb-4">
+          <v-row>
+            <v-col cols="3" class="d-flex align-center pa-4">
+              <v-img
+                :src="member.path"
+                height="250"
+                cover
+                class="bg-grey-lighten-2 rounded-circle mx-auto"
+              />
+            </v-col>
+            <v-col cols="8">
+              <v-card-title style="color: #f4b754" class="pb-0"
+                >{{ member.name }}
+              </v-card-title>
+              <p class="pl-4 font-weight-bold">
+                {{ member.role }}
+              </p>
+              <v-card-text v-if="member.description">
+                {{
+                  showFullDescription[index]
+                    ? member.description
+                    : member.description.slice(0, 500) + "..."
+                }}
+              </v-card-text>
+              <v-card-text v-else class="text-grey"
+                >Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Sapiente veritatis omnis ab, exercitationem fuga nobis quos
+                harum commodi nemo voluptatem amet id adipisci ducimus
+                consectetur expedita dolore nostrum quis maxime!
+              </v-card-text>
+              <span v-if="member.description.length > 500">
+                <v-btn
+                  class="pa-0 mx-3 bg-transparent"
+                  color="blue"
+                  variant="text"
+                  @click="toggleDescription(index)"
+                >
+                  {{ showFullDescription[index] ? "Show less" : "Show more" }}
+                </v-btn>
+              </span>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
+import { data } from "@/assets/scripts/teamdata.js";
+import { ref } from "vue";
 
+const showFullDescription = ref(Array(data.length).fill(false));
 
+const toggleDescription = (index) => {
+  showFullDescription.value[index] = !showFullDescription.value[index];
+};
 </script>
