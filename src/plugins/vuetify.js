554677347c2bee9c5c0c51ff29126d65bb8b3d34
Sean Benson Guerra <sean_c_benson@outlook.com>
2025-02-04 10:54:40 +0000

    added a couple themes for the company colors.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,15 +5,33 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+
+const mainTheme = {
+  dark: false,
+  colors: {
+    background: "#9C27B0",
+  },
+};
+
+const secondaryTheme = {
+  dark: false,
+  colors: {
+    background: "#f4b754",
+  },
+};
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: "mainTheme",
+    themes: {
+      mainTheme,
+      secondaryTheme,
+    },
   },
-})
+});
