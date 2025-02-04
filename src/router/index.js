bcb98c5013f2cfb34312a9a51e1792c7a17e5b94
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-04 18:48:27 +0000

    imported the registration vue component to the router object.

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Team from "@/components/team/index.vue";
 import About from "@/components/about/index.vue";
 import Training from "@/components/training/index.vue";
 import Blog from "@/components/blog/index.vue";
+import Register from "@/components/users/register.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -12,6 +13,7 @@ const router = createRouter({
     { path: "/about", component: About },
     { path: "/training", component: Training },
     { path: "/blog", component: Blog },
+    { path: "/register", component: Register },
   ],
 });
 
