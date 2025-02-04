de273645d40a996917df3eace97eea49f3c762b8
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-04 13:35:09 +0000

    Placed a hero image and title.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,21 @@
-<template></template>
+<template>
+    <v-sheet class="hero-section" height="100vh">
+        <v-container class="h-100 d-flex align-center">
+            <div class="text-center w-100">
+                <h1 class="text-h1 text-white font-weight-bold">Online Language Learning for Business Success</h1>
+                <h2 class="text-h2 text-white">Join a vibrant community!</h2>
+                <v-btn>Contact Us</v-btn>
+            </div>
+        </v-container>
+    </v-sheet>
+</template>
 
 <script setup></script>
+<style scoped>
+.hero-section{
+    background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),url('@/assets/hero_placeholder.webp');
+    background-size: cover;
+    background-position: center;
+    width: 100%;
+}
+</style>
