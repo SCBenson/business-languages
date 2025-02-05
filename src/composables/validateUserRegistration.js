30091ff96303537d1e5200b7286ea3a83418fbf4
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-05 14:44:17 +0000

    first commit for newly installed dependencies for using firebase and firestore, and pinia.

---
+++ src/composables/validateUserRegistration.js
@@ -0,0 +1,30 @@
+import { ref, reactive } from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { email, required, minLength, sameAs } from "@vuelidate/validators";
+
+export function useValidateUserRegistration() {
+  const showPassword = ref(false);
+
+  const formData = reactive({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const rules = {
+    formData: {
+      name: { required, minLength: minLength(4) },
+      email: { required, email },
+      password: { required, minLength: minLength(6) },
+      confirmPassword: { required, sameAsPassword: sameAs(formData.password) },
+    },
+  };
+  const v$ = useVuelidate(rules, { formData });
+
+  return {
+    formData,
+    showPassword,
+    v$,
+  };
+}
