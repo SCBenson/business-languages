bf1c0d1defd1ab35284210cd26a8353e8e0a24f5
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-05 14:45:19 +0000

    imported pinia and mounted pinia to the app.

--- src/main.js
+++ src/main.js
@@ -7,15 +7,17 @@
 // Plugins
 import { registerPlugins } from "@/plugins";
 import router from "@/router/index.js";
+import { createPinia } from "pinia";
 
 // Components
 import App from "./App.vue";
 
 // Composables
 import { createApp } from "vue";
-
+const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
+app.use(pinia);
 
 registerPlugins(app);
 
