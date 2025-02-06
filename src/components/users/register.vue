41a60f0106eda46c6c5381d574d28226604b6deb
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-06 18:11:23 +0000

    revised the validate function. Imported the composable.

--- src/components/users/register.vue
+++ src/components/users/register.vue
@@ -14,7 +14,6 @@
                   "
                   label="Name"
                   required
-                  @input="v$.formData.name.$touch()"
                   @blur="v$.formData.name.$touch()"
                   prepend-icon="mdi-account-circle"
                 />
@@ -29,7 +28,7 @@
                   "
                   label="Email"
                   required
-                  @input="v$.formData.email.$touch()"
+
                   @blur="v$.formData.email.$touch()"
                   prepend-icon="mdi-mail"
                 />
@@ -45,7 +44,6 @@
                   "
                   label="Password"
                   required
-                  @input="v$.formData.password.$touch()"
                   @blur="v$.formData.password.$touch()"
                   prepend-icon="mdi-lock"
                   :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
@@ -64,7 +62,14 @@
     </v-card>
   </v-container>
 </template>
-<script>
+<script setup>
+import { useValidateUserRegistration } from '@/composables/validateUserRegistration';
+const {formData, v$, showPassword} = useValidateUserRegistration();
+
+const register = async () => {
+  const isFormCorrect = await v$.value.$validate();
+  if(!isFormCorrect) return
+};
 // import {useValidateUserRegistration} from '@/composables/validateUserRegistration.js'
 // import {useRouter} from 'vue-router'
 // import {defineStore} from 'pinia'
@@ -86,64 +91,64 @@
 //     console.log(err)
 //   }
 // }
-import { useVuelidate } from "@vuelidate/core";
-import { required, minLength, email, sameAs } from "@vuelidate/validators";
+// import { useVuelidate } from "@vuelidate/core";
+// import { required, minLength, email, sameAs } from "@vuelidate/validators";
 
-export default {
-  setup() {
-    // v$ is reactive
-    const v$ = useVuelidate();
-    return { v$ };
-    // Common Properties to Check Against for Validation
-    // "$dirty": false "Boolean that triggers once the user types into the field"
-    // "$error": false "Boolean that is true if $invalid && $dirty == true"
-    // "$errors": [] "Array of errors"
-    // "$silentErrors": []
-    // "$invalid:" false "Indicates the state of validation when any of its child validators "
-  },
-  data() {
-    return {
-      formData: {
-        name: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      },
-      showPassword: false,
-    };
-  },
-  validations() {
-    return {
-      formData: {
-        name: { required, minLength: minLength(4) },
-        email: { required, email },
-        password: { required, minLength: minLength(6) },
-        confirmPassword: {
-          required,
-          sameAsPassword: sameAs(this.formData.password),
-        },
-      },
-    };
-  },
-  methods: {
-    async register() {
-      //$validate() cross-references the current state of the validator, according to validations()
-      const isFormCorrect = await this.v$.$validate();
-      if (!isFormCorrect) return;
+// export default {
+//   setup() {
+//     // v$ is reactive
+//     const v$ = useVuelidate();
+//     return { v$ };
+//     // Common Properties to Check Against for Validation
+//     // "$dirty": false "Boolean that triggers once the user types into the field"
+//     // "$error": false "Boolean that is true if $invalid && $dirty == true"
+//     // "$errors": [] "Array of errors"
+//     // "$silentErrors": []
+//     // "$invalid:" false "Indicates the state of validation when any of its child validators "
+//   },
+//   data() {
+//     return {
+//       formData: {
+//         name: "",
+//         email: "",
+//         password: "",
+//         confirmPassword: "",
+//       },
+//       showPassword: false,
+//     };
+//   },
+//   validations() {
+//     return {
+//       formData: {
+//         name: { required, minLength: minLength(4) },
+//         email: { required, email },
+//         password: { required, minLength: minLength(6) },
+//         confirmPassword: {
+//           required,
+//           sameAsPassword: sameAs(this.formData.password),
+//         },
+//       },
+//     };
+//   },
+//   methods: {
+//     async register() {
+//       //$validate() cross-references the current state of the validator, according to validations()
+//       const isFormCorrect = await this.v$.$validate();
+//       if (!isFormCorrect) return;
 
-      this.$store
-        .dispatch("register", {
-          name: this.formData.name,
-          email: this.formData.email,
-          password: this.formData.password,
-        })
-        .then(() => {
-          this.$router.push({ name: "Dashboard" });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-  },
-};
+//       this.$store
+//         .dispatch("register", {
+//           name: this.formData.name,
+//           email: this.formData.email,
+//           password: this.formData.password,
+//         })
+//         .then(() => {
+//           this.$router.push({ name: "Dashboard" });
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//         });
+//     },
+//   },
+// };
 </script>
