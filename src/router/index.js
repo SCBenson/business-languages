44ff6963ff5759e4f037f2a3902f4bce6c8be48a
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-09 12:52:41 +0000

    added a route to the dashboard.

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Training from "@/components/training/index.vue";
 import Blog from "@/components/blog/index.vue";
 import Register from "@/components/users/register.vue";
 import Login from "@/components/users/login.vue";
+import Dashboard from "@/components/dashboard/dashboard.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +17,7 @@ const router = createRouter({
     { path: "/blog", component: Blog },
     { path: "/register", component: Register },
     { path: "/login", component: Login },
+    { path: "/dashboard", component: Dashboard },
   ],
 });
 
