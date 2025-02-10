786c146c38ecc8bf957d5b2417793f41ac752857
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-10 14:28:48 +0000

    creating and exporting a config object to define the base for deploying to GitHub Pages.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
 };
+
+export default defineConfig({
+  base: "/business-languages/",
+  plugins: [vue()],
+});
