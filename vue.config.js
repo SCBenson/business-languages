d2e5de1ea1d6f5f00cee5b94ac5df3f80b891f7c
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-10 15:53:41 +0000

    accidently created a config object to act as vite.mjs config. Since removed it.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
 };
-
-export default defineConfig({
-  base: "/business-languages/",
-  plugins: [vue()],
-});
