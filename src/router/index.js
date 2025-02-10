95ee87ee142ac6fc7831c520d7e39d472abd8b40
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-10 14:36:55 +0000

    changed the router links. [login & register -> registeration]

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,8 @@ import Team from "@/components/team/index.vue";
 import About from "@/components/about/index.vue";
 import Training from "@/components/training/index.vue";
 import Blog from "@/components/blog/index.vue";
-import Register from "@/components/users/register.vue";
-import Login from "@/components/users/login.vue";
+import Register from "@/components/users/registration.vue";
+
 import Dashboard from "@/components/dashboard/dashboard.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,8 +15,7 @@ const router = createRouter({
     { path: "/about", component: About },
     { path: "/training", component: Training },
     { path: "/blog", component: Blog },
-    { path: "/register", component: Register },
-    { path: "/login", component: Login },
+    { path: "/registration", component: Register },
     { path: "/dashboard", component: Dashboard },
   ],
 });
