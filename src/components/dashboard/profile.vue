f0dca06f022256e496f800dd1e7994163472a778
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-12 14:15:45 +0000

    added a form to hold inputs for the user to fill out and edit their profile. WORK IN PROGRESS.

--- src/components/dashboard/profile.vue
+++ src/components/dashboard/profile.vue
@@ -1,6 +1,77 @@
 <template>
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-        <p>Profile</p>
+    <v-main class="ma-0 pa-0">
+        
+        <v-card>
+            <h1>Profile Information</h1>
+
+            <v-form>
+                <v-row>
+                    <v-col class="text-center d-flex align-center justify-center">
+                        <p>First Name: </p>
+                    </v-col>
+                    
+                    <v-col>
+
+                        <v-text-field
+                        width="350"
+                        disabled
+                        readonly
+                        variant="outlined"
+                        class="mx-auto"
+
+                        > Sean
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <v-divider></v-divider>
+                <v-row>
+                    <v-col class="text-center d-flex align-center justify-center">
+                        <p>Last Name: </p>
+                    </v-col>
+                    
+                    <v-col>
+
+                        <v-text-field
+                        width="350"
+                        disabled
+                        readonly
+                        variant="outlined"
+                        class="mx-auto"
+                        > Benson
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <v-divider></v-divider>
+                <v-row>
+                    <v-col class="text-center d-flex align-center justify-center">
+                        <p>Date of Birth: </p>
+                    </v-col>
+
+                    <v-col>
+
+                        <v-text-field
+                        width="350"
+                        disabled
+                        readonly
+                        variant="outlined"
+                        class="mx-auto"
+                        > 04/01/1998
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <v-divider></v-divider>
+                <v-row>
+                    <v-col></v-col>
+                    <v-col class="text-center">
+                        <v-btn
+                        color="green">Edit</v-btn>
+                    </v-col>
+                </v-row>
+                
+            </v-form>
+
+
+        </v-card>
     </v-main>
 </template>
 <script setup>
