f4f113fbefb6b7aee6ff7fb5f98dde0278e96057
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-12 14:07:15 +0000

    removed justify and align classes in v-main.

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -31,7 +31,7 @@
             </v-navigation-drawer>
 
 
-            <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+            <v-main class="d-flex" style="min-height: 300px;">
                 <component :is="currentComponent"></component>
             </v-main>
         </v-layout>
