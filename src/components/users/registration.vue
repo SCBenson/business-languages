acb6d30f02e89a162e9c3d80a384d1de7ec9898d
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-13 15:46:46 +0000

    made the user signOut when registering; added confirm password field for registration; Fixed bug when trying to switch methods from registration to login; creates and fills out a new document at registration; at login, it fetches relevant user data and places it in a pinia state store

--- src/components/users/registration.vue
+++ src/components/users/registration.vue
@@ -12,11 +12,11 @@
                   <v-text-field
                     v-model="formData.name"
                     :error-messages="
-                      v$.formData.name.$errors?.map((e) => e.$message) || []
+                      v$?.formData?.name.$errors?.map((e) => e.$message) || []
                     "
                     label="Name"
                     required
-                    @blur="v$.formData.name.$touch()"
+                    @blur="v$?.formData?.name?.$touch()"
                     prepend-icon="mdi-account-circle"
                   />
                 </v-col>
@@ -26,12 +26,12 @@
                   <v-text-field
                     v-model="formData.email"
                     :error-messages="
-                      v$.formData.email.$errors?.map((e) => e.$message) || []
+                      v$?.formData?.email.$errors?.map((e) => e.$message) || []
                     "
                     label="Email"
                     required
   
-                    @blur="v$.formData.email.$touch()"
+                    @blur="v$?.formData?.email?.$touch()"
                     prepend-icon="mdi-mail"
                   />
                 </v-col>
@@ -42,11 +42,28 @@
                     v-model="formData.password"
                     :type="showPassword ? 'text' : 'password'"
                     :error-messages="
-                      v$.formData.password.$errors?.map((e) => e.$message) || []
+                      v$?.formData?.password.$errors?.map((e) => e.$message) || []
                     "
                     label="Password"
                     required
-                    @blur="v$.formData.password.$touch()"
+                    @blur="v$?.formData?.password?.$touch()"
+                    prepend-icon="mdi-lock"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword = !showPassword"
+                  />
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col v-if="registering">
+                  <v-text-field
+                    v-model="formData.confirmPassword"
+                    :type="showPassword ? 'text' : 'password'"
+                    :error-messages="
+                      v$?.formData?.confirmPassword.$errors?.map((e) => e.$message) || []
+                    "
+                    label="Confirm Password"
+                    required
+                    @blur="v$?.formData?.confirmPassword?.$touch()"
                     prepend-icon="mdi-lock"
                     :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
                     @click:append="showPassword = !showPassword"
@@ -91,20 +108,26 @@
     </v-container>
   </template>
   <script setup>
-  import { ref } from 'vue'
+  import { ref, nextTick } from 'vue'
+  import { DB, AUTH } from "@/firebase/config.js";
+  import { signOut } from "firebase/auth";
+  import { doc, getDoc, setDoc } from "firebase/firestore";
+  import { useUserStore } from '@/composables/stores/userStore.js';
   import { useAuthValidation } from '@/composables/validateUser.js';
   import { useFirebaseAuth } from '@/composables/useFirebaseAuth.js'
   import { useRouter } from 'vue-router';
   import { errorMessages } from '@/composables/authErrorMessaging.js';
+
   
-  const registering= ref(false);
-  const { formData, showPassword, v$, resetForm } = useAuthValidation();
+  const userStore = useUserStore();
+  const { formData, showPassword, v$, resetForm, isRegistering: registering } = useAuthValidation();
   const { registerUser, loginUser } = useFirebaseAuth();
   const router = useRouter();
   const snackbar = ref(false);
   const snackbarColor = ref('error');
   const snackbarMessage = ref('');
   const userLoggedIn = ref(false);
+
   //========================User-friendly mapping for the Firebase Error Codes========================//
   const getErrorMessage = (method, errorCode) => {
     if(method == 'register'){
@@ -114,12 +137,16 @@
     }
   };
 
-  const switchMethod = () => {
-    registering.value = !registering.value;
+  const switchMethod = async () => {
     resetForm();
-    v$.value.$reset();
-    // formData.email = '';
-    // formData.password = '';
+
+    registering.value = !registering.value;
+
+    await nextTick();
+    if (v$.value){
+      v$.value.$reset();
+    }
+    
   }
 
   //========================Input Form Validation using Vuelidate=====================================//
@@ -137,6 +164,11 @@
     console.log("Register function called")
     const isFormCorrect = await v$.value.$validate();
     console.log('Form validation result:', isFormCorrect);
+    console.log('All validation errors:', v$.value.$errors);
+    console.log('Name errors:', v$.value.formData.name?.$errors);
+    console.log('Email errors:', v$.value.formData.email?.$errors);
+    console.log('Password errors:', v$.value.formData.password?.$errors);
+    console.log('Form Data:', formData);
     if(!isFormCorrect) {
       console.log('Form validation failed');
       return;
@@ -154,10 +186,29 @@
       console.log("Registration failed:", error.message);
       return;
     }
+    //Add information to firestore DB.
+    try{
+      const usersCollection = doc(DB, 'users', user.uid);
+
+      await setDoc(usersCollection, {
+        'first-name': formData.name,
+        'last-name': '',
+        'languages': '',
+        'profile-picture': '',
+        'email': formData.email,
+        'uid': user.uid,
+      });
+      
+    } catch(error) {
+      console.log('Error submitting the user information: ', error);
+    }
+
+
     snackbarColor.value = 'success';
     snackbar.value = true;
     snackbarMessage.value = "Registration successful! Redirecting...";
     console.log("Successfully registered:", user);
+    signOut(AUTH);
     //set a timer for the snackbar to exist before redirecting...
     setTimeout(() => {
       router.replace('/').catch(err => {
@@ -181,11 +232,26 @@
 //========================UI Alert to notify user of unsuccessful/successful registration===========//
     if(error) {
         snackbarColor.value = 'error';
-        snackbarMessage.value = getErrorMessage('register', error.code);
+        snackbarMessage.value = getErrorMessage('login', error.code);
         snackbar.value = true;
         console.log("Login failed:", error.message);
         return;
     }
+
+    try{
+      const userDocRef = doc(DB, 'users', user.uid);
+      const userDoc = await getDoc(userDocRef);
+
+      if(userDoc.exists()) {
+        userStore.setUserDetails(userDoc.data());
+        console.log("User data fetched:", userDoc.data());
+      } else {
+        console.log("No user document found!");
+      }
+    }catch(error){
+      console.error("Error fetching user data:", fetchError);
+    }
+
     snackbarColor.value = 'success';
     snackbar.value = true;
     userLoggedIn.value = false;
@@ -198,6 +264,7 @@
         });
     }, 3000);
   };
+
 
   </script>
   
