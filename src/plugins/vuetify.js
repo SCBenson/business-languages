7743f4e5e9fec024543a1a071ffaf846efdea5f7
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-20 12:13:55 +0000

    Deleted a theme and edited the main theme to the orange color.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,15 +14,8 @@ import { createVuetify } from "vuetify";
 const mainTheme = {
   dark: false,
   colors: {
-    background: "#E09DEC",
-    "custom-gold": "#f4b754",
-  },
-};
-
-const secondaryTheme = {
-  dark: false,
-  colors: {
     background: "#f4b754",
+    "custom-gold": "#f4b754",
   },
 };
 
