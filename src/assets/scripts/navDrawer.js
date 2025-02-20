59f7a25a502682acfa25e0f347d626b989c81a38
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-02-20 12:14:24 +0000

    added a new javascript file to house the navigation drawer information for the header component.

---
+++ src/assets/scripts/navDrawer.js
@@ -0,0 +1,13 @@
+export const data = {
+  menuItems: [
+    { title: "Home", path: "/", icon: "mdi-home" },
+    { title: "Team", path: "/team", icon: "mdi-account-group" },
+    { title: "About", path: "/about", icon: "mdi-information" },
+    { title: "Blog", path: "/blog", icon: "mdi-post" },
+    {
+      title: "BLA Interactive",
+      path: "/bla-interactive",
+      icon: "mdi-domain",
+    },
+  ],
+};
