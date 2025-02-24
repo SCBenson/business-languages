406989746f8bd235ff64641464d54ce505d60d1f
Sean Benson Guerra <sean_c_benson@outlook.com>
2025-02-24 13:18:24 +0000

    imported and introduced the Footer component into the root component for rendering.

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
     <v-main>
       <router-view />
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script setup>
 import Header from "@/components/header/index.vue";
+import Footer from "@/components/footer/index.vue";
 </script>
