efa971797c9c7d86e9689f2b88945ecbc213c777
Sean Benson Guerra <sean_c_benson@outlook.com>
2025-02-25 16:39:27 +0000

    added language lessons and business and guiness sections.

--- src/assets/scripts/navDrawer.js
+++ src/assets/scripts/navDrawer.js
@@ -5,9 +5,19 @@ export const data = {
     { title: "About", path: "/about", icon: "mdi-information" },
     { title: "Blog", path: "/blog", icon: "mdi-post" },
     {
+      title: "Training Lessons",
+      path: "/training-lessons",
+      icon: "mdi-pencil",
+    },
+    {
       title: "BLA Interactive",
       path: "/bla-interactive",
       icon: "mdi-domain",
+    },
+    {
+      title: "Business & Guiness",
+      path: "/business-and-guiness",
+      icon: "mdi-microphone",
     },
   ],
 };
