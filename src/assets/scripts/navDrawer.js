d2ba32be5eb4644e37882e863e9e33b5e9166cd7
Sean Benson Guerra <sean_c_benson@outlook.com>
2025-03-03 15:55:06 +0000

    imported photos for new pages.

--- src/assets/scripts/navDrawer.js
+++ src/assets/scripts/navDrawer.js
@@ -5,19 +5,40 @@ export const data = {
     { title: "About", path: "/about", icon: "mdi-information" },
     { title: "Blog", path: "/blog", icon: "mdi-post" },
     {
-      title: "Training Lessons",
-      path: "/training-lessons",
-      icon: "mdi-pencil",
-    },
-    {
       title: "BLA Interactive",
       path: "/bla-interactive",
       icon: "mdi-domain",
     },
     {
-      title: "Business & Guiness",
-      path: "/business-and-guiness",
+      title: "Lessons",
+      path: "",
+      icon: "mdi-pencil",
+      sublinks: [
+        { title: "Online Trainings", path: "/online-trainings" },
+        { title: "Language Trainings", path: "/training-lessons" },
+        { title: "Translations", path: "/translations" },
+        { title: "Learning Trips", path: "/learning-trips" },
+        { title: "Online Tips", path: "/online-tips" },
+      ],
+    },
+
+    {
+      title: "Media",
+      path: "",
       icon: "mdi-microphone",
+      sublinks: [
+        { title: "Business and Guiness", path: "/business-and-guiness" },
+        { title: "Webseries", path: "/webseries" },
+      ],
+    },
+    {
+      title: "Legal",
+      path: "",
+      icon: "",
+      sublinks: [
+        { title: "Disclaimer", path: "/disclaimer" },
+        { title: "Data Protection", path: "/data-protection" },
+      ],
     },
   ],
 };
---
+++ src/assets/scripts/onlinetrainingdata.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    title: "New Chapter",
+    img: "src/assets/online-training/img1.webp",
+    color: "#e5e5e5",
+    description:
+      "Years and years ago, Business Languages Augsburg had the great idea of offering courses online, using the Second Life virtual online platform. Lots of money was invested and the concept was brilliant. What happened? Our customers didn’t want it. Fast forward to March 2020. Suddenly everyone wanted and indeed needed online training. After ignoring the concept for the years in-between, we were faced with adapting or going under. And we adapted and can now offer cutting edge training online.",
+  },
+  {
+    title: "2020/2021 - Speed!",
+    img: "src/assets/online-training/img2.webp",
+    color: "#e5e5e5",
+    description:
+      "Having used 2020 to get up to speed, we can use whatever tool your company is already familiar with (Microsoft Teams, Skype, Slack, Flock, Workplace by Facebook, Google Drive, Bria teams, you name it). You can use your PC, Laptop, phone, tablet, fridge or whatever you use to connect to the internet. We develop individual training concepts specifically geared towards your company or business sector to deliver a specialist, practically oriented language training, at the time of your choosing. We offer single or small group training. We have modular trainings and workshop trainings. Here are some examples.",
+  },
+  {
+    title: "Modular trainings: Business English",
+    img: "src/assets/online-training/img3.webp",
+    color: "#e5e5e5",
+    description:
+      "These tend not to be weekly or bi-weekly like the examples above, but rather one time a month for a full or half day. A single topic will be dealt with e.g. presentations or negotiations, technical English or English for online trade fairs. The scope of the individual modules will be designed specifically for your company. A self-study program can be developed for the time in between the modules.",
+  },
+  {
+    title: "Workshops",
+    img: "src/assets/online-training/img4.webp",
+    color: "#e5e5e5",
+    description:
+      "Normally a full day training on topics as varied as a virtual tour of production incorporating product training and dealing with questions or perhaps telephoning with an emphasis on customer care. Please see our program for details.",
+  },
+  {
+    title: "German training, single lessons",
+    img: "src/assets/online-training/img5.webp",
+    color: "#e5e5e5",
+    description:
+      "A lot of businesspeople from abroad work in Germany and need to develop their German for their work. We arrange a virtual meeting to ascertain level (if any) and goals. The trainer will offer time slots and make recommendations as to how frequent the training should be. Homework can be sent electronically if deemed beneficial, classes can be recorded for later review, new vocabulary is imparted using chat functions. But as with all of our classes the emphasis will be on communication. We look at cultural aspects like when to use Sie or Du. We offer advice on everyday tasks like opening bank accounts and shopping. But most importantly we will act as a sounding board and practice partner for your business exchanges.",
+  },
+  {
+    title: "Basic Mandarin: Group training",
+    img: "src/assets/online-training/img6.webp",
+    color: "#e5e5e5",
+    description:
+      "The group should be small, (4 participants is recommended), homogenous (it doesn’t help to have beginners in a group with proficient speakers) and target oriented. We will set up the groups in consultation with participants, superiors and personnel departments, after a grading interview online or by phone. Active participation will be encouraged at all times. Practical exercises will be offered. And we won’t bullshit you. You will learn the basics, polite phrases, numbers, reading signs, booking into hotels, welcoming guests etc. But proficiency in Mandarin will require total immersion, so do keep that in mind.",
+  },
+];
