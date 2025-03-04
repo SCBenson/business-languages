f176c906fbf2367f791933d16f383dba4eac6446
Sean Benson Guerra <sean_c_benson@outlook.com>
2025-03-04 13:03:48 +0000

    minor edit

--- src/components/about/index.vue
+++ src/components/about/index.vue
@@ -5,7 +5,7 @@
       <v-col cols="12">
         <v-card color="#f4b754">
           <v-card-title class="text-custom-purple">Global Team</v-card-title>
-          <v-card-text class="text-custom-white"
+          <v-card-text class="text-white"
             >Our language professionals span the globe and are here to help you
             excel in your second or third language.</v-card-text
           >
@@ -16,7 +16,7 @@
           <v-card-title class="text-custom-purple"
             >More Than Just Language.</v-card-title
           >
-          <v-card-text class="text-custom-white"
+          <v-card-text class="text-white"
             >We understand cultural differences and current business
             trends.</v-card-text
           >
@@ -27,7 +27,7 @@
           <v-card-title class="text-custom-purple"
             >Industry-Specific Training</v-card-title
           >
-          <v-card-text class="text-custom-white"
+          <v-card-text class="text-white"
             >We believe having trainers who understand your field as paramount.
             Our trainers have taken on technical to CEO level roles.
           </v-card-text>
@@ -36,9 +36,10 @@
       <v-col cols="12">
         <v-card color="#f4b754">
           <v-card-title class="text-custom-purple"
-            >Specializing in Business English</v-card-title
+            >Specializing in Business <br />
+            English</v-card-title
           >
-          <v-card-text class="text-custom-white"
+          <v-card-text class="text-white"
             >We offer additional services in German and other major languages.
           </v-card-text>
         </v-card>
@@ -48,7 +49,7 @@
           <v-card-title class="text-custom-purple"
             >Support for Newcomers</v-card-title
           >
-          <v-card-text class="text-custom-white"
+          <v-card-text class="text-white"
             >We have been very sucessful in training new arrivals to companies
             in the German language. We will support you in learning both the
             German language and business culture.
@@ -60,7 +61,7 @@
           <v-card-title class="text-custom-purple"
             >Translation Services</v-card-title
           >
-          <v-card-text class="text-custom-white"
+          <v-card-text class="text-white"
             >We provide translation services, as well as certified translations
           </v-card-text>
         </v-card>
