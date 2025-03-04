a4f688cb1dd8b4f350a6322f25d2aee3d76bfaf8
Sean Benson Guerra <sean_c_benson@outlook.com>
2025-03-04 13:05:52 +0000

    added a responsive header, still needs more work, but it changes from sandwich icon xs screen to a router link view after medium screen sizes

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -14,9 +14,47 @@
         Business Languages
       </v-toolbar-title>
     </template>
+    <v-spacer></v-spacer>
+
+    <v-container class="d-none d-md-flex justify-end align-center gap-4">
+      <router-link to="/training" class="mx-2 text-white font-weight-bold"
+        >Get Training
+      </router-link>
+      <router-link to="/" class="mx-2 text-white">Home</router-link>
+      <router-link to="/team" class="mx-2 text-white">Team</router-link>
+      <router-link to="/about" class="mx-2 text-white">About Us</router-link>
+      <router-link to="/blog" class="mx-2 text-white">Blog</router-link>
+
+      <!-- <router-link to="/registration" class="mx-2 text-white"
+        >Register</router-link
+      > -->
+      <router-link
+        v-if="isAuth == null"
+        to="/registration"
+        class="mx-2 text-white"
+        >Login</router-link
+      >
+      <router-link
+        v-if="isAuth != null"
+        to="/"
+        @click="handleSignOut"
+        class="mx-2 text-white"
+        >Logout</router-link
+      >
+      <v-card
+        v-if="isAuth != null"
+        density="compact"
+        prepend-avatar="https://randomuser.me/api/portraits/women/10.jpg"
+        subtitle="Member"
+        :title="profileData.firstName"
+        variant="text"
+      >
+      </v-card>
+    </v-container>
 
     <template v-slot:append>
       <v-app-bar-nav-icon
+        class="d-flex d-md-none"
         variant="text"
         color="white"
         @click.stop="drawer = !drawer"
@@ -72,14 +110,6 @@
   </v-navigation-drawer> -->
 </template>
 
-<script setup>
-import logoUrl from "@/assets/bl-anniversary-logo.png";
-import { data } from "@/assets/scripts/navDrawer.js";
-
-import { ref } from "vue";
-
-const drawer = ref(false);
-</script>
 <style scoped>
 .router-link-active,
 a,
@@ -95,64 +125,47 @@ p:hover {
 }
 </style>
 
-<!-- <v-container class="d-flex justify-end align-center gap-4"> -->
-<!-- <router-link to="/training" class="mx-2 text-white font-weight-bold"
-        >Get Training
-      </router-link> -->
-<!-- <router-link to="/" class="mx-2 text-white">Home</router-link>
-      <router-link to="/team" class="mx-2 text-white">Team</router-link>
-      <router-link to="/about" class="mx-2 text-white">About Us</router-link>
-      <router-link to="/blog" class="mx-2 text-white">Blog</router-link> -->
-<!-- TODO: mode conditional using route and onMounted methods in registration.vue to direct to /registration?mode=register OR /registration?mode=login  -->
-<!-- <router-link to="/registration" class="mx-2 text-white">Register</router-link>
-      <router-link v-if="isAuth == null" to="/registration" class="mx-2 text-white">Login</router-link>
-      <router-link v-if="isAuth != null" to="/" @click="handleSignOut" class="mx-2 text-white">Logout</router-link>
-      <v-card v-if="isAuth != null"
-        density="compact"
-        prepend-avatar="https://randomuser.me/api/portraits/women/10.jpg"
-        subtitle="Member"
-        :title="profileData.firstName"
-        variant="text"
-      >
-      </v-card> -->
-<!-- </v-container> -->
-
 <!-- Script for Desktop Version -->
-<!-- import { useUserStore } from "@/composables/stores/userStore.js";
-    import { AUTH } from '@/firebase/config.js';
-    import { signOut, onAuthStateChanged } from 'firebase/auth';
-    import { ref, onMounted } from 'vue';
-  //check if the user is logged in...
-    const isAuth = ref(AUTH.currentUser);
-  
-    const userStore = useUserStore();
-  // Create local reactive data
-    const profileData = ref({
-      firstName: '',
-      lastName: '',
-      email: '',
-      languages: ''
-    });
-  
-  
-    // Update local data when component mounts
-    onMounted(() => {
-        if (userStore.userDetails) {
-          console.log("userStore entered")
-            profileData.value = {
-                firstName: userStore.userDetails['first-name'] || '',
-                lastName: userStore.userDetails['last-name'] || '',
-                email: userStore.userDetails.email || '',
-                languages: userStore.userDetails.languages || ''
-            };
-        }
-    });
-    const handleSignOut = () => {
-      //sign out the user...
-      signOut(AUTH);
-    }
-  
-    onAuthStateChanged(AUTH, (user) => {
-      console.log(user);
-      isAuth.value = user;
-    }) -->
+<script setup>
+import { useUserStore } from "@/composables/stores/userStore.js";
+import { AUTH } from "@/firebase/config.js";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { ref, onMounted } from "vue";
+import logoUrl from "@/assets/bl-anniversary-logo.png";
+import { data } from "@/assets/scripts/navDrawer.js";
+
+const drawer = ref(false);
+//check if the user is logged in...
+const isAuth = ref(AUTH.currentUser);
+
+const userStore = useUserStore();
+// Create local reactive data
+const profileData = ref({
+  firstName: "",
+  lastName: "",
+  email: "",
+  languages: "",
+});
+
+// Update local data when component mounts
+onMounted(() => {
+  if (userStore.userDetails) {
+    console.log("userStore entered");
+    profileData.value = {
+      firstName: userStore.userDetails["first-name"] || "",
+      lastName: userStore.userDetails["last-name"] || "",
+      email: userStore.userDetails.email || "",
+      languages: userStore.userDetails.languages || "",
+    };
+  }
+});
+const handleSignOut = () => {
+  //sign out the user...
+  signOut(AUTH);
+};
+
+onAuthStateChanged(AUTH, (user) => {
+  console.log(user);
+  isAuth.value = user;
+});
+</script>
