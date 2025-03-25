e7969314eaff4d19fbe252f3239acbfd37dba9f5
Sean Benson Guerra <sean_c_benson@outlook.com>
2025-03-25 14:30:30 +0000

    about page is now fully translated.

--- src/components/about/index.vue
+++ src/components/about/index.vue
@@ -1,73 +1,69 @@
 <template>
   <v-container class="text-center px-8">
-    <h1 class="text-custom-purple">About Us</h1>
+    <h1 class="text-custom-purple">{{ $t("about.title.title") }}</h1>
     <v-row>
       <v-col cols="12">
         <v-card color="#f4b754">
-          <v-card-title class="text-custom-purple">Global Team</v-card-title>
-          <v-card-text class="text-white"
-            >Our language professionals span the globe and are here to help you
-            excel in your second or third language.</v-card-text
-          >
+          <v-card-title class="text-custom-purple">{{
+            $t("about.global-team.title")
+          }}</v-card-title>
+          <v-card-text class="text-black">{{
+            $t("about.global-team.description")
+          }}</v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12">
         <v-card color="#f4b754">
-          <v-card-title class="text-custom-purple"
-            >More Than Just Language.</v-card-title
-          >
-          <v-card-text class="text-white"
-            >We understand cultural differences and current business
-            trends.</v-card-text
-          >
+          <v-card-title class="text-custom-purple">{{
+            $t("about.more-than-just-language.title")
+          }}</v-card-title>
+          <v-card-text class="text-black">{{
+            $t("about.more-than-just-language.description")
+          }}</v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12">
         <v-card color="#f4b754">
-          <v-card-title class="text-custom-purple"
-            >Industry-Specific Training</v-card-title
-          >
-          <v-card-text class="text-white"
-            >We believe having trainers who understand your field as paramount.
-            Our trainers have taken on technical to CEO level roles.
+          <v-card-title class="text-custom-purple">{{
+            $t("about.industry-specific-training.title")
+          }}</v-card-title>
+          <v-card-text class="text-black"
+            >{{ $t("about.industry-specific-training.description") }}
           </v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12">
         <v-card color="#f4b754">
-          <v-card-title class="text-custom-purple"
-            >Specializing in Business <br />
-            English</v-card-title
-          >
-          <v-card-text class="text-white"
-            >We offer additional services in German and other major languages.
+          <v-card-title class="text-custom-purple">{{
+            $t("about.specializing-in-business-english.title")
+          }}</v-card-title>
+          <v-card-text class="text-black"
+            >{{ $t("about.specializing-in-business-english.description") }}
           </v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12">
         <v-card color="#f4b754">
-          <v-card-title class="text-custom-purple"
-            >Support for Newcomers</v-card-title
-          >
-          <v-card-text class="text-white"
-            >We have been very sucessful in training new arrivals to companies
-            in the German language. We will support you in learning both the
-            German language and business culture.
+          <v-card-title class="text-custom-purple">{{
+            $t("about.support-for-newcomers.title")
+          }}</v-card-title>
+          <v-card-text class="text-black"
+            >{{ $t("about.support-for-newcomers.description") }}
           </v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12">
         <v-card color="#f4b754">
-          <v-card-title class="text-custom-purple"
-            >Translation Services</v-card-title
-          >
-          <v-card-text class="text-white"
-            >We provide translation services, as well as certified translations
+          <v-card-title class="text-custom-purple">{{
+            $t("about.translation-services.title")
+          }}</v-card-title>
+          <v-card-text class="text-black"
+            >{{ $t("about.translation-services.description") }}
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
-    <v-btn class="mt-4" color="purple">Book Now</v-btn>
+    <v-btn class="mt-4" color="purple">{{ $t("about.title.button") }}</v-btn>
   </v-container>
 </template>
 
