859979edd242a026ab49b8e51a8cf458492c655c
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-03-27 15:34:02 +0000

    Translations page is now responsive for mobile and desktop.

--- src/components/translations/index.vue
+++ src/components/translations/index.vue
@@ -2,43 +2,69 @@
   <v-component>
     <v-row class="text-center">
       <v-col>
-        <h1 class="mt-4">{{ $t("translations.title.title") }}</h1>
+        <h1 class="mt-4 text-custom-purple">{{ $t("translations.title.title") }}</h1>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="12">
         <v-card class="mx-8 mb-4">
-          <v-row class="text-left">
-            <v-col cols="12" md="6">
-              <v-img src="@/assets/translations/img1.webp"></v-img>
+          <v-row class="text-left" style="min-height: 100%">
+            <v-col cols="12" md="6" class="d-flex pa-0">
+              <v-img cover height="100%" src="@/assets/translations/img1.webp"></v-img>
             </v-col>
-            <v-col cols="12" md="6">
-              <v-card-title class="text-center">{{
+   
+            <v-col cols="12" md="6" class="pa-0 d-md-flex align-md-center">
+              <div class="mx-6">
+              <v-card-title class="text-center responsive-title">{{
                 $t("translations.card.title")
               }}</v-card-title>
 
-              <p class="px-4">
+              <p class="px-4 responsive-text">
                 {{ $t("translations.card.paragraph1") }}<br /><br />
                 {{ $t("translations.card.paragraph2") }} <br /><br />
                 {{ $t("translations.card.paragraph3") }}
               </p>
-              <p class="px-4" v-if="$t('translations.card.paragraph4')">
+              <p class="px-4 responsive-text" v-if="$t('translations.card.paragraph4')">
                 <br />
                 {{ $t("translations.card.paragraph4") }}
               </p>
-              <p class="px-4" v-if="$t('translations.card.paragraph5')">
+              <p class="px-4 responsive-text" v-if="$t('translations.card.paragraph5')">
                 <br />
                 {{ $t("translations.card.paragraph5") }}
               </p>
               <div class="text-center d-flex justify-center">
-                <v-btn class="my-4 text-black" color="#f4b754">{{
+                <v-btn class="my-4 text-black responsive-text" color="#f4b754">{{
                   $t("translations.card.button")
                 }}</v-btn>
               </div>
+            </div>
             </v-col>
+           
           </v-row>
         </v-card>
       </v-col>
     </v-row>
   </v-component>
 </template>
+
+<style scoped>
+.responsive-title {
+  font-size: 1.1rem;
+  font-weight: 700;
+}
+
+.responsive-text {
+  font-size: 0.95rem;
+}
+
+@media (min-width: 960px) {
+  .responsive-title {
+    font-size: 1.4rem;
+    font-weight: 700;
+  }
+  
+  .responsive-text {
+    font-size: 1.1rem;
+  }
+}
+</style>
