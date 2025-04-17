b628b85777a0a0d5697ac38162d5502764a3ca2a
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-04-17 19:22:11 +0100

    working on vite config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/business-languages/" : "/",
-  plugins: [vue()],
+  base: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
 });
 // import { defineConfig } from "vite";
 // import vue from "@vitejs/plugin-vue";
