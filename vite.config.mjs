cd308d19b470ca2cdaf41a0dd044fbc41cf7da22
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-04-18 00:01:44 +0100

    added text compression with brotli and bundle size optimization. Along with a visualizer to generate a visual representation of my bundle size.

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,8 @@ import Components from 'unplugin-vue-components/vite'
 import Vue from '@vitejs/plugin-vue'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import ViteFonts from 'unplugin-fonts/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import viteCompression from 'vite-plugin-compression'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -26,6 +28,21 @@ export default defineConfig({
         }],
       },
     }),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+
+    process.env.NODE_ENV === 'production' && visualizer({
+      open: true,
+      filename: 'stats.html',
+      gzipSize: true,
+      brotliSize: true,
+    })
   ],
   define: { 'process.env': {} },
   resolve: {
@@ -52,4 +69,43 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    target: 'es2015',
+    reportCompressedSize: true,
+    chunkSizeWarningLimit: 1000,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules/vue/') || id.includes('node_modules/vue-router/')) {
+            return 'vue-core';
+          }
+          
+          if (id.includes('node_modules/vuetify/')) {
+            return 'vuetify';
+          }
+          
+          if (id.includes('node_modules/@mdi/font/')) {
+            return 'icons';
+          }
+          
+          if (id.includes('src/locales/')) {
+            return 'locales';
+          }
+          
+          if (id.includes('node_modules/') && !id.includes('node_modules/vue/') && 
+              !id.includes('node_modules/vue-router/') && !id.includes('node_modules/vuetify/') && 
+              !id.includes('node_modules/@mdi/font/')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
