7dcb1adb8b76e096ef642bbb8f207400027b7de2
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-04-17 19:24:09 +0100

    reverting my vite config to module.exports

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,3 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
-});
-// import { defineConfig } from "vite";
-// import vue from "@vitejs/plugin-vue";
-
-// module.exports = {
-//   publicPath:
-//     process.env.NODE_ENV === "production" ? "/business-languages/" : "/",
-//   plugins: [vue()],
-// };
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+};
