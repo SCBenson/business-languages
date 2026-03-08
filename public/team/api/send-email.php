15cafe1ec052867b26aa036948278bcaa9db9e6a
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-03-08 12:52:26 -0500

    added cors headers at the top of the PHP mailer file.

--- public/team/api/send-email.php
+++ public/team/api/send-email.php
@@ -1,9 +1,17 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: https://www.businesslanguages.de/api/send-email.php');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST'){
     http_response_code(405);
     echo json_encode(['error' => 'Method not allowed!']);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'OPTIONS'){
+    http_response_code(204);
     exit;
 }
 
